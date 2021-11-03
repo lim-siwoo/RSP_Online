27fb5c6a53f0f1c8e3ba332dff3bd1061b4f21f1
임시우 (Siwoo Lim) <siwoo5530@gmail.com>
2021-11-03 20:46:52 +0900

    MainLobby

---
+++ src/MainLobby.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class MainLobby {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton button1;
+    private JButton button2;
+}
